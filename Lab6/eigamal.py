ff15ebf579eb0a3ba2a4bb4ffad26b23f2ab4d18
feliciaL3 <113386223+feliciaL3@users.noreply.github.com>
2023-12-12 00:54:54 +0200

    6

---
+++ Lab6/eigamal.py
@@ -0,0 +1,53 @@
+# Import necessary libraries
+import hashlib
+import random
+import math
+
+# Define the message
+msg = "Lupascu Felicia"
+
+# Hash the message using SHA3-384
+hash_object = hashlib.sha3_384()
+hash_object.update(msg.encode())
+hashed_message = int.from_bytes(hash_object.digest(), byteorder='big')
+
+# Ensure the hash has a specific bit length
+hash_size = 512
+hashed_message = hashed_message << (hash_size - hashed_message.bit_length())
+print("Hashed message:", hashed_message)
+
+# Choose large prime 'p' and generator 'g'
+p = 32317006071311007300153513477825163362488057133489075174588434139269806834136210002792056362640164685458556357935330816928829023080573472625273554742461245741026202527916572972862706300325263428213145766931414223654220941111348629991657478268034230553086349050635557712219187890332729569696129743856241741236237225197346402691855797767976823014625397933058015226858730761197532436467475855460715043896844940366130497697812854295958659597567051283852132784468522925504568272879113720098931873959143374175837826000278034973198552060607533234122603254684088120031105907484281003994966956119696956248629032338072839127039
+g = 2
+
+# Choose private key 'a' randomly
+a = random.randint(1, p - 2)
+
+# Compute public key 'b'
+b = pow(g, a, p)
+
+# Loop to generate a valid random value 'k'
+while True:
+    # Generate a random value 'k'
+    k = random.randint(1, p - 2)
+    gcd_value = math.gcd(k, p - 1)
+
+    # Ensure 'k' is coprime with 'p-1'
+    if gcd_value == 1:
+        # Calculate signature components 'r' and 's'
+        r = pow(g, k, p)
+        s = (pow(k, -1, p - 1) * (hashed_message - a * r)) % (p - 1)
+        signature = (r, s)
+        print("Signature:", signature)
+
+        # Save the generated signature for verification
+        received_signature = signature
+        r_received, s_received = received_signature
+
+        # Verify the signature
+        v1 = (pow(b, r_received, p) * pow(r_received, s_received, p)) % p
+        v2 = pow(g, hashed_message, p)
+        verification = (v1 == v2)
+
+        print("Signature Verification:", verification)
+        break
---
+++ Lab6/rsa.py
@@ -0,0 +1,62 @@
+# Import necessary libraries
+import random
+import hashlib
+from sympy import isprime
+import math
+
+# Function to generate a large prime number with a given number of bits
+def generate_large_prime(bits):
+    while True:
+        num = random.getrandbits(bits)
+        num |= (1 << bits - 1) | 1  # Ensure the number is odd and has the correct bit length
+
+        if isprime(num):
+            return num
+
+# Function to choose a public exponent for RSA
+def choose_public_exponent(phi_n):
+    e = 65537  # Commonly used public exponent
+
+    while not (1 < e < phi_n and math.gcd(e, phi_n) == 1):
+        e = random.randint(2, phi_n - 1)
+
+    return e
+
+# Message to be signed
+msg = "Lupascu Felicia"
+print("Message:", msg)
+
+# Hash the message using SHA3-512
+hash_object = hashlib.sha3_512()
+# Update the hash object with the encoded message
+hash_object.update(msg.encode())
+hashed_message = int.from_bytes(hash_object.digest(), byteorder='big')
+
+# Ensure the hash has a specific bit length
+hash_size = 512
+hashed_message = hashed_message << (hash_size - hashed_message.bit_length())
+print("Hashed message:", hashed_message)
+
+# Choose two large prime numbers
+bits = 1554
+prime1 = generate_large_prime(bits)
+prime2 = generate_large_prime(bits)
+
+# Calculate the modulus and Euler's totient function
+n = prime1 * prime2
+phi_n = (prime1 - 1) * (prime2 - 1)
+
+# Choose a public exponent
+e = choose_public_exponent(phi_n)
+
+# Calculate the private exponent
+d = pow(e, -1, phi_n)
+
+# Calculate the digital signature using the private exponent
+signature = pow(hashed_message, d, n)
+
+# Verify the signature using the public exponent
+verification = pow(signature, e, n)
+
+# Print the result of signature verification
+print("Signature Verification Result:", verification == hashed_message)
